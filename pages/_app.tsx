b92f9e05cc3670d2c2a7ef38813151e24efd026f
FarooqiEarning <farooqi.earning.official@gmail.com>
2025-11-03 18:29:26 +0500

    feat: update footer content and add service worker registration
    Updated the footer to reflect new copyright information for 2025 and updated the attribution to "Converso Empire". Added service worker registration in `_app.tsx` to enable offline capabilities and improve performance.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,18 @@
 import { AppProps } from "next/app";
+import { useEffect } from "react";
 
 import "../styles/global.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js').catch((err) => {
+          console.error('Service worker registration failed:', err);
+        });
+      });
+    }
+  }, []);
+
   return <Component {...pageProps} />;
 }
